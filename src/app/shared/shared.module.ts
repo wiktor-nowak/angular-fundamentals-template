fd03544dc579201849776f0d155920cb7f7be143
wiktor-nowak <nowak.wiktor95@gmail.com>
2024-10-02 05:23:05 +0200

    forms, and others fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -16,7 +16,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { DurationPipe } from './pipes/duration.pipe';
 import { CustomDatePipe } from './pipes/custom-date.pipe';
 import { EmailValidatorDirective } from '@shared/directives/email.directive';
-import { TogglePasswordDirective } from './directives/toggle.directive';
+import { TogglePasswordDirective } from '@shared/directives/toggle.directive';
 
 const components = [
   HeaderComponent,
@@ -31,7 +31,7 @@ const components = [
   DurationPipe,
   CustomDatePipe,
   EmailValidatorDirective,
-  TogglePasswordDirective
+  TogglePasswordDirective,
 ];
 
 @NgModule({
